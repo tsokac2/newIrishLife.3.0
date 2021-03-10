aa25febe47bb2d4b46b9b39a935c9f8a7fbfda91
tsokac2 <tsokac2@gmail.com>
2021-03-10 17:31:13 +0000

    Add app.config variables in forms.py

--- forms.py
+++ forms.py
@@ -10,3 +10,11 @@ from wtforms.validators import (
 from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
