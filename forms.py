92fd6be2801d745512010bd773b21c18ba388f2c
tsokac2 <tsokac2@gmail.com>
2021-03-10 17:29:42 +0000

    Add Flask MongoDB imports in forms.py

--- forms.py
+++ forms.py
@@ -4,3 +4,7 @@ from flask_wtf import FlaskForm
 from wtforms import(
     StringField, PasswordField, SubmitField,
     TextAreaField, SelectField)
+from wtforms.validators import (
+    DataRequired, Length, Regexp,
+    Email, EqualTo, ValidationError)
+from flask_pymongo import PyMongo
