a1d667ea58440417e87032af39567ef816fadad2
tsokac2 <tsokac2@gmail.com>
2021-03-10 17:28:06 +0000

    Add wtforms.validators imports in forms.py

--- forms.py
+++ forms.py
@@ -1,3 +1,6 @@
 import os
 from flask import Flask, request, session
 from flask_wtf import FlaskForm
+from wtforms import(
+    StringField, PasswordField, SubmitField,
+    TextAreaField, SelectField)
