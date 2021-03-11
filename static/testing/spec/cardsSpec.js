932380b3d2f47e000877c3fb6fda422fef6110a0
tsokac2 <tsokac2@gmail.com>
2021-03-11 21:03:47 +0000

    Add #Card2 test event function in cardsSpec.js

--- static/testing/spec/cardsSpec.js
+++ static/testing/spec/cardsSpec.js
@@ -26,6 +26,30 @@ describe("Testing DOM Elements - Triggering .on('click') Event in trip.html, wor
         expect(spyEventHide).toHaveBeenTriggered();
     });
 
-    
+    it("Should invoke the jQuery click event when elements #showLinks2 and #hideLinks2 are clicked", function(){
+        setFixtures('<button id="showLinks2" class="load-links-btn">Quick Links...<i class="fas fa-sort-down"></i></button>');
+        $('#showLinks2').on('click', function (){
+            var thisCardLinkShow = "." + this.id + "-grid";
+                $(thisCardLinkShow).show(300);
+                $(".blur").fadeIn(400);
+                $(thisCardLinkShow).addClass("rel-card");
+                $("#Card2").addClass("wrap-rel");
+        });
+
+        setFixtures(' <button id="hideLinks2" class="hideLinks-btn"><i class="far fa-times-circle"></i></button>');
+        $("#hideLinks2").on("click", function (){
+            var thisCardLinkShow = "." + this.id + "-grid";
+                $(thisCardLinkShow).hide(400);
+                $(".blur").fadeOut(300);
+                $("#Card2").removeClass("wrap-rel");
+        });
+
+        var spyEventShow = spyOnEvent('#showLinks2', 'click');
+        var spyEventHide = spyOnEvent('#hideLinks2', 'click');
+        $('#showLinks2').trigger('click');
+        $('#hideLinks2').trigger('click');
+        expect(spyEventShow).toHaveBeenTriggered();
+        expect(spyEventHide).toHaveBeenTriggered();
+    });
 
 });
