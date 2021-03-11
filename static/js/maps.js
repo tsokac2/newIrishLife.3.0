b5eaa2ec0186115a864780b449dd7078922fdcf1
tsokac2 <tsokac2@gmail.com>
2021-03-11 19:48:25 +0000

    Configure initMap Google API function in maps.js

---
+++ static/js/maps.js
@@ -0,0 +1,12 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom : 11,
+        center: {
+            lat: 53.350140,
+            lng: -6.266155
+        }
+    });
+
+    
+
+}
