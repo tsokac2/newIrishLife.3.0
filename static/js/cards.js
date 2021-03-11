798eeb374ae11449470d4b760ee961f0e63dc3c6
tsokac2 <tsokac2@gmail.com>
2021-03-11 18:31:09 +0000

    Add on click DOM event for #Card2 in cards.js

--- static/js/cards.js
+++ static/js/cards.js
@@ -16,3 +16,22 @@ $(".blur").on("click", function  () {
         $("#Card1").removeClass("wrap-rel");
         $(".hideLinks1-grid").hide(400);
 });
+
+$("#showLinks2").on("click", function (){
+    var thisCardLinkShow = "." + this.id + "-grid";
+        $(thisCardLinkShow).show(300);
+        $(".blur").fadeIn(400);
+        $(thisCardLinkShow).addClass("rel-card");
+        $("#Card2").addClass("wrap-rel");
+});
+$("#hideLinks2").on("click", function (){
+    var thisCardLinkShow = "." + this.id + "-grid";
+        $(thisCardLinkShow).hide(400);
+        $(".blur").fadeOut(300);
+        $("#Card2").removeClass("wrap-rel");
+});
+$(".blur").on("click", function () {
+        $(".blur").fadeOut(300);
+        $("#Card2").removeClass("wrap-rel");
+        $(".hideLinks2-grid").hide(400);
+});
