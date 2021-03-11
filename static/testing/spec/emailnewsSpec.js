b3a9841e6a7d5d5a2cd4bb54de5d97e4b39f16df
tsokac2 <tsokac2@gmail.com>
2021-03-11 21:12:32 +0000

    Add test email function in emailNewsSpec.js

--- static/testing/spec/emailnewsSpec.js
+++ static/testing/spec/emailnewsSpec.js
@@ -16,6 +16,23 @@ describe("Testing Callback functions for Validation process against Input Elemen
         });
     });
 
+    describe("Validate Email", function(){
+        it("validateEmail() function should exist", function(){
+            expect(validateEmail).toBeDefined();
+        });
+
+        it("validateEmail should return TRUE", function(){
+            var testPass = validateName("email@gmail.com");
+            expect(testPass).toBe(true); 
+        });
+
+        it("validateEmail should return FALSE", function(){
+            var testFaild = validateName("");
+            expect(testFaild).toBe(false); 
+        });
+
+    });
+
     
 
 });
