75450e1d39da310cc8c7d344a54aff8aec3d51c1
tsokac2 <tsokac2@gmail.com>
2021-03-15 08:21:25 +0000

    Fix spelling errors forms.py file

--- forms.py
+++ forms.py
@@ -23,24 +23,24 @@ mongo = PyMongo(app)
 class RegisterForm(FlaskForm):
     username = StringField(
         "username", validators=[DataRequired("Enter your username!"), Length(
-            min=2, message="Must be at least more than 1 character"),
+            min=2, message="Must be at least more than 1 character!"),
             Length(max=15, message="Max 15 characters!"), Regexp(
-                "^\w+$", message="Only letters numbers or underscore")],
+                "^\w+$", message="Only letters numbers or underscore!")],
         render_kw={"placeholder": "Username"})
 
     email = StringField("email", validators=[DataRequired("Enter your email"),
-                        Email("An email address must contain a single @")],
+                        Email("An email address must contain a single @!")],
                         render_kw={"placeholder": "Email"})
 
     password = PasswordField(
         "password", [DataRequired("Enter a password"), Length(
-            min=6, message="Password must be at least 6 character")],
+            min=6, message="Password must be at least 6 characters!")],
         render_kw={"placeholder": "Password"})
 
     confirm_password = PasswordField(
         "Confirm Password", [DataRequired("Confirm password"), EqualTo(
-            "password", "Passwords must match")],
-        render_kw={"placeholder": "Confirm Password"})
+            "password", "Passwords must match!")],
+        render_kw={"placeholder": "Confirm Password!"})
 
     submit = SubmitField("SIGN UP")
 
@@ -48,7 +48,7 @@ class RegisterForm(FlaskForm):
         user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
         if user:
-            raise ValidationError("Username is taken, choose a different one")
+            raise ValidationError("Username is taken, choose a different one!")
 
     def validate_email(self, email):
         email = mongo.db.users.find_one({"email": request.form.get("email")})
@@ -60,7 +60,7 @@ class LoginForm(FlaskForm):
 
     username = StringField(
         "username", validators=[DataRequired(
-            "Enter your username")],
+            "Enter your username!")],
         render_kw={"placeholder": "Your username"})
 
     password = PasswordField(
@@ -73,15 +73,15 @@ class LoginForm(FlaskForm):
 
 class AddTip(FlaskForm):
     tip_title = StringField(
-        "tip_title", validators=[DataRequired("Enter a tip title"), Length(
-            min=5, message="Must be more that 5 character!"), Length(
+        "tip_title", validators=[DataRequired("Enter a tip title!"), Length(
+            min=5, message="Must be more than 5 characters!"), Length(
                 max=50, message="Max length 50 characters!")],
         render_kw={"placeholder": "Tip Title"})
 
     tip_description = TextAreaField(
         "tip_description", validators=[DataRequired(
             "Enter a tip description"), Length(
-                min=10, message="Must be more that 10 characters!"), Length(
+                min=10, message="Must be more than 10 characters!"), Length(
                     max=500, message="Max length 300 characters!")],
         render_kw={"placeholder": "Tip Description"})
 
